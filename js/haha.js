8990db92a3d8bafa2f1f1a10ef068216852f5496
dongjize <justecommeca@163.com>
2018-04-27 17:08:48 +1000

    new version

--- js/haha.js
+++ js/haha.js
@@ -1,56 +1,9 @@
-// var d1 = new Date("2016-03-28 10:17");
-// var d2 = new Date("2016-03-28 12:17");
-// console.log(d2 > d1); //两个时间相差的毫秒数
-// console.log(d2 - d1); //两个时间相差的毫秒数
-// console.log(parseInt(d2 - d1) / 1000); //两个时间相差的秒数
-// console.log(parseInt(d2 - d1) / 1000 / 60); //两个时间相差的分钟数
-// console.log(parseInt(d2 - d1) / 1000 / 60); //两个时间相差的小时数
+var date1 = new Date("2018/05/31 19:40:00");
+var date2 = new Date("2018/05/31 19:47:00");
 
-// var arr = [1, 2, 3, 4, 5, 6, 7];
-// var haha = [6, 7, 8];
-// fuck = arr.slice(0, 3) + arr.slice(4, arr.length);
-// console.log(fuck);
+console.log(date1.valueOf());
+console.log(date1.valueOf() + 100000);
+console.log(new Date(date1.valueOf() + 100000));
 
-// Array.prototype.removeByValue = function(val) {
-//     for (var i = 0; i < this.length; i++) {
-//         if (this[i] == val) {
-//             this.splice(i, 1);
-//             break;
-//         }
-//     }
-// };
-
-// var somearray = ["mon", "tue", "wed", undefined, "thur"];
-// console.log(somearray);
-// somearray.removeByValue(undefined);
-// console.log(somearray);
-
-// var d = new Date("2018-03-08T05:56:00.000Z");
-// console.log(d);
-
-// var father = [1, 3, 5, 7, 9, 11, 13, 15];
-// var mother = [2, 4, 6, 8, 10, 12, 14, 16];
-
-// var len = mother.length;
-
-// var ca = Math.floor(Math.random() * len);
-// var cb = Math.floor(Math.random() * len);
-// if (ca > cb) {
-//     var tmp = cb;
-//     cb = ca;
-//     ca = tmp;
-// }
-// var son = father
-//     .slice(0, ca)
-//     .concat(mother.slice(ca, cb - ca))
-//     .concat(father.slice(cb, len));
-// var daughter = mother
-//     .slice(0, ca)
-//     .concat(father.slice(ca, cb - ca))
-//     .concat(mother.slice(cb, len));
-
-// console.log(father);
-// console.log(mother);
-// console.log(son);
-// console.log(daughter);
-
+var a = [1, 2, 3, 4, 5, 6, 7];
+console.log(a[-1]);
