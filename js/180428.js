b67cb216e80bdf90691d9117001138c8e7d7ee18
dongjize <justecommeca@163.com>
2018-05-08 11:40:42 +1000

    bug fix

--- js/180428.js
+++ js/180428.js
@@ -1,8 +1,8 @@
 var dataset = {
     eventId: 10248228,
     participantObjectId: 10115412,
-    countTeachers: 3,
     sessionInterval: 10,
+    countTeachers: 4,
     teacherSessions: [
         {
             teacher: {
@@ -10,238 +10,256 @@ var dataset = {
                 userFullName: "David Eedle"
             },
             countSessions: 12,
-            sessionInterval: 10,
             sessions: [
                 {
                     sessionId: 118,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:07:00"
+                    required: 0
                 },
                 {
                     sessionId: 119,
+                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:17:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:17:00"
+                    required: 0
                 },
                 {
                     sessionId: 120,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:27:00"
+                    required: 0
                 },
                 {
                     sessionId: 121,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:37:00"
+                    required: 0
                 },
                 {
                     sessionId: 122,
+                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:47:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:47:00"
+                    required: 0
                 },
                 {
                     sessionId: 123,
+                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:57:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:57:00"
+                    required: 0
                 },
                 {
                     sessionId: 124,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:07:00"
+                    required: 0
                 },
                 {
                     sessionId: 125,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:17:00"
+                    required: 0
                 },
                 {
                     sessionId: 126,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:27:00"
+                    required: 0
                 },
                 {
                     sessionId: 127,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:37:00"
+                    required: 0
                 },
                 {
                     sessionId: 128,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:47:00"
+                    required: 0
                 },
                 {
                     sessionId: 129,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
                     teacher: {
                         userId: 107023,
                         userFullName: "David Eedle"
                     },
-                    required: 1,
-                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:57:00"
+                    required: 0
                 }
             ],
-            required: 1
+            required: 0
         },
         {
             teacher: {
                 userId: 107240,
                 userFullName: "Marlee B'atterham"
             },
-            countSessions: 10,
-            sessionInterval: 10,
+            countSessions: 12,
             sessions: [
                 {
                     sessionId: 142,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:07:00"
+                    required: 0
                 },
                 {
                     sessionId: 143,
+                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:17:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:17:00"
+                    required: 0
                 },
                 {
                     sessionId: 144,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:27:00"
+                    required: 0
                 },
                 {
                     sessionId: 145,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:37:00"
+                    required: 0
                 },
                 {
                     sessionId: 146,
+                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:47:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:47:00"
+                    required: 0
                 },
                 {
                     sessionId: 147,
+                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:57:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
-                    sessionEndDateTimeLocal: "2018/05/31 18:57:00"
+                    required: 0
+                },
+                {
+                    sessionId: 148,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 149,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
                 },
                 {
                     sessionId: 150,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:07:00"
+                    required: 0
                 },
                 {
                     sessionId: 151,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:17:00"
+                    required: 0
                 },
                 {
                     sessionId: 152,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:27:00"
+                    required: 0
                 },
                 {
                     sessionId: 153,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
                     teacher: {
                         userId: 107240,
                         userFullName: "Marlee B'atterham"
                     },
-                    required: 0,
-                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
-                    sessionEndDateTimeLocal: "2018/05/31 19:37:00"
+                    required: 0
                 }
             ],
             required: 0
@@ -376,6 +394,136 @@ var dataset = {
                 }
             ],
             required: 1
+        },
+        {
+            teacher: {
+                userId: 107241,
+                userFullName: "Jeong-eun Kim"
+            },
+            countSessions: 12,
+            sessions: [
+                {
+                    sessionId: 142,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 143,
+                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:17:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 144,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 145,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 146,
+                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:47:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 147,
+                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:57:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 148,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 149,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 150,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 151,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 152,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 153,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                }
+            ],
+            required: 0
         }
     ]
 };
--- js/haha.js
+++
@@ -1,9 +0,0 @@
-var date1 = new Date("2018/05/31 19:40:00");
-var date2 = new Date("2018/05/31 19:47:00");
-
-console.log(date1.valueOf());
-console.log(date1.valueOf() + 100000);
-console.log(new Date(date1.valueOf() + 100000));
-
-var a = [1, 2, 3, 4, 5, 6, 7];
-console.log(a[-1]);
