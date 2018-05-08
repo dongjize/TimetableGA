7b458b503960c05db875c57405659c9959a0d007
dongjize <justecommeca@163.com>
2018-05-08 12:05:06 +1000

    add occupied sessions

---
+++ js/180428_1.js
@@ -0,0 +1,429 @@
+var dataset = {
+    eventId: 10248228,
+    participantObjectId: 10115412,
+    sessionInterval: 10,
+    countTeachers: 4,
+    teacherSessions: [
+        {
+            teacher: {
+                userId: 107023,
+                userFullName: "David Eedle"
+            },
+            countSessions: 9,
+            sessions: [
+                {
+                    sessionId: 118,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 120,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 121,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 124,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 125,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 126,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 127,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 128,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 129,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
+                    teacher: {
+                        userId: 107023,
+                        userFullName: "David Eedle"
+                    },
+                    required: 0
+                }
+            ],
+            required: 0
+        },
+        {
+            teacher: {
+                userId: 107240,
+                userFullName: "Marlee B'atterham"
+            },
+            countSessions: 11,
+            sessions: [
+                {
+                    sessionId: 142,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 143,
+                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:17:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 144,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 145,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 146,
+                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:47:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 148,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 149,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 150,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 151,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 152,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 153,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Marlee B'atterham"
+                    },
+                    required: 0
+                }
+            ],
+            required: 0
+        },
+        {
+            teacher: {
+                userId: 107028,
+                userFullName: "David Bowie"
+            },
+            countSessions: 10,
+            sessionInter: 10,
+            sessions: [
+                {
+                    sessionId: 120,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00"
+                },
+                {
+                    sessionId: 121,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00"
+                },
+                {
+                    sessionId: 122,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 18:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:47:00"
+                },
+                {
+                    sessionId: 123,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 18:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:57:00"
+                },
+                {
+                    sessionId: 124,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:07:00"
+                },
+                {
+                    sessionId: 125,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:17:00"
+                },
+                {
+                    sessionId: 126,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00"
+                },
+                {
+                    sessionId: 127,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00"
+                },
+                {
+                    sessionId: 128,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00"
+                },
+                {
+                    sessionId: 129,
+                    teacher: {
+                        userId: 107028,
+                        userFullName: "David Bowie"
+                    },
+                    required: 1,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00"
+                }
+            ],
+            required: 1
+        },
+        {
+            teacher: {
+                userId: 107241,
+                userFullName: "Jeong-eun Kim"
+            },
+            countSessions: 8,
+            sessions: [
+                {
+                    sessionId: 142,
+                    sessionStartDateTimeLocal: "2018/05/31 18:00:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:07:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 143,
+                    sessionStartDateTimeLocal: "2018/05/31 18:10:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:17:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 144,
+                    sessionStartDateTimeLocal: "2018/05/31 18:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:27:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 145,
+                    sessionStartDateTimeLocal: "2018/05/31 18:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 18:37:00",
+                    teacher: {
+                        userId: 107240,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 150,
+                    sessionStartDateTimeLocal: "2018/05/31 19:20:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:27:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 151,
+                    sessionStartDateTimeLocal: "2018/05/31 19:30:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:37:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 152,
+                    sessionStartDateTimeLocal: "2018/05/31 19:40:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:47:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                },
+                {
+                    sessionId: 153,
+                    sessionStartDateTimeLocal: "2018/05/31 19:50:00",
+                    sessionEndDateTimeLocal: "2018/05/31 19:57:00",
+                    teacher: {
+                        userId: 107241,
+                        userFullName: "Jeong-eun Kim"
+                    },
+                    required: 0
+                }
+            ],
+            required: 0
+        }
+    ]
+};
